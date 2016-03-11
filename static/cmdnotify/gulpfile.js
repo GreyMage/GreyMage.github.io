83320ac62084764ecca9540c93953987972a460b
Eric Cutler <ecutler@cmdagency.com>
2016-03-11 14:33:48 -0800

    testing forced updates

--- static/cmdnotify/gulpfile.js
+++ static/cmdnotify/gulpfile.js
@@ -9,21 +9,21 @@ var gulp = require('gulp'),
 	
 	
 gulp.task('js', function() {
-  return gulp.src('script.js', { style: 'expanded' })
+  return gulp.src('src/script.js', { style: 'expanded' })
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
 		.pipe(uglify())
 		.pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('dist'))
 });
 	
 gulp.task('css', function() {
-  return gulp.src('style.less', { style: 'expanded' })
+  return gulp.src('src/style.less', { style: 'expanded' })
     .pipe(less())
     .pipe(autoprefixer('last 2 version'))
 		.pipe(cssnano({zindex: false}))
 		.pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('default', [], function() {
