83a62bcf7d376055af1e31ba5b9814fa18a567a5
Eric Cutler <bulzeeb@gmail.com>
2018-07-26 14:23:52 -0700

    add userscript entry point

---
+++ static/reddit/sanity.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name     Reddit Sanity
+// @description  With Russia propoganda and bots flooding reddit, this script aims to help identify users posts better, by including identicons, account age, and hate-sub posting warnings.
+// @version  1
+// @grant    none
+// @include  http://old.reddit.com/*
+// @include  https://old.reddit.com/*
+// @include  http://www.reddit.com/*
+// @include  https://www.reddit.com/*
+// ==/UserScript==
+
+// Inject Code from repo.
+const s = document.createElement("script");
+s.src = "//greymage.github.io/static/reddit/build/app.js";
+document.body.appendChild(s);
