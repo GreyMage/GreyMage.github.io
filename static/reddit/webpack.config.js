532373533adadcdb783f1ac22cd3e426c8e2f0ff
Cades <bulzeeb@gmail.com>
2018-07-27 21:00:37 -0700

    dev mode for a bit

--- static/reddit/webpack.config.js
+++ static/reddit/webpack.config.js
@@ -37,26 +37,26 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                screw_ie8: true,
-                conditionals: true,
-                unused: true,
-                comparisons: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                if_return: true,
-                join_vars: true
-            },
-            output: {
-                comments: false
-            }
-        }),
-        new webpack.HashedModuleIdsPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        })
+        // new webpack.optimize.UglifyJsPlugin({
+            // compress: {
+                // warnings: false,
+                // screw_ie8: true,
+                // conditionals: true,
+                // unused: true,
+                // comparisons: true,
+                // sequences: true,
+                // dead_code: true,
+                // evaluate: true,
+                // if_return: true,
+                // join_vars: true
+            // },
+            // output: {
+                // comments: false
+            // }
+        // }),
+        // new webpack.HashedModuleIdsPlugin(),
+        // new webpack.DefinePlugin({
+            // 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        // })
     ]
 }
